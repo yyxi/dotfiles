70f8a0343cd12dd551a961322721d1feb2210e79
yyxi <178086254+yyxi@users.noreply.github.com>
2022-05-25 17:25:11 +0000

    fix: remove .config/fish before install

--- install.sh
+++ install.sh
@@ -4,9 +4,5 @@ if [ -n "$GITPOD_WORKSPACE_ID" ]; then
   sudo apt-add-repository -y ppa:fish-shell/release-3
   sudo apt install -y fish
 
-  if [[ -d "${HOME}/.config/fish" ]]; then
-    rm -rf "${HOME}/.config/fish"
-  fi
-
   "${HOME}/.dotfiles/manage" install
 fi
