6f5866eaa4fc4f3aad2541a2ec1fc8473553a436
yyxi <178086254+yyxi@users.noreply.github.com>
2022-11-09 23:05:20 +0100

    fix: paths

--- fish/config.fish
+++ fish/config.fish
@@ -46,15 +46,15 @@ if test -d /opt/homebrew/opt/curl/bin
 end
 
 if test -d /usr/local/bin
-  fish_add_path /opt/homebrew/bin
-end
-
-if test -d /usr/local/sbin
-  fish_add_path /opt/homebrew/sbin
+  fish_add_path /usr/local/bin
 end
 
 if test -d /opt/homebrew/bin
   fish_add_path /opt/homebrew/bin
+end
+
+if test -d /usr/local/sbin
+  fish_add_path /usr/local/sbin
 end
 
 if test -d /opt/homebrew/sbin
