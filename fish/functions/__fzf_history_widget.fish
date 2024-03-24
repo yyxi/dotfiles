f388ad8fa419acfaf0a61f3db6c17b9b164f0933
yyxi <178086254+yyxi@users.noreply.github.com>
2024-03-24 13:38:37 +0000

    fix: fish history

--- fish/functions/__fzf_history_widget.fish
+++ fish/functions/__fzf_history_widget.fish
@@ -1,4 +1,5 @@
 function __fzf_history_widget -d "Show command history"
+  history merge
   begin
     set -lx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS --tiebreak=index --bind=ctrl-r:toggle-sort,ctrl-z:ignore +m"
     history -z | eval fzf --read0 --print0 -q '(commandline)' | read -lz result
