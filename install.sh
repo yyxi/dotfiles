d941dc260876eb4ca86b50bdcc5326799e154138
yyxi <178086254+yyxi@users.noreply.github.com>
2026-05-19 11:34:49 +0000

    fix: replace legacy manage installer with python cli

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -n "$GITPOD_WORKSPACE_ID" ]; then
-  sudo apt-add-repository -y ppa:fish-shell/release-3
-  sudo apt install -y fish
-
-  "${HOME}/.dotfiles/manage" install
-fi
