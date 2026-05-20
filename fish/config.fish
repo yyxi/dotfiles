67f011cfd1f3d24a2b1d1a8341471e5a79dab526
yyxi <178086254+yyxi@users.noreply.github.com>
2026-05-20 04:19:12 +0000

    fix: add doggo

--- fish/config.fish
+++ fish/config.fish
@@ -29,6 +29,7 @@ set -x GREP_COLORS "mt=$GREP_COLOR"
 set -x FZF_DEFAULT_OPTS "--color bw"
 set -gx FZF_DEFAULT_COMMAND  'rg --files --color never --hidden --glob \'!.git\''
 set -x _ZO_FZF_OPTS "--color bw"
+set -x FX_THEME "0"
 
 alias ls="ls -a -l -h --group-directories-first --color=auto"
 
